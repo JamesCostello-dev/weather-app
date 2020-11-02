155562b90e68b0d5343776e3b03cf5e65144303d
James Costello <jcotab@gmail.com>
2020-11-01 23:50:05 -0700

    fetch forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const apiKey = 'e3b8bd27aaa26d31be8e66dd93093f79';
 const citySubmit = document.querySelector('#city-submit');
 const cityInput = document.querySelector('#city-input');
 
@@ -16,47 +17,60 @@ const citySubmitHanlder = (event) => {
   } else {
     alert('Please enter a city.');
   }
-  console.log(event);
+  //console.log(event);
 };
 
 // display current weather
 const displayCurrentWeather = (data) => {
 
-  let list = document.querySelector('ul');
+  let currentCity = document.querySelector('#city-name');
+
+  let displayCurrentCity = data.name;
+  currentCity.textContent = displayCurrentCity;
 
   let displayCurrentTemp = data.main;
-  let currentTemp = document.createElement('li');
+  let currentTemp = document.createElement('h5');
   currentTemp.textContent = 'Temperature: ' + displayCurrentTemp.temp + ' °F';
-  list.appendChild(currentTemp);
+  currentCity.appendChild(currentTemp);
 
   let displayCurrentHumidity = data.main;
-  let currentHumidity = document.createElement('li');
+  let currentHumidity = document.createElement('h5');
   currentHumidity.textContent = 'Humidity: ' + displayCurrentHumidity.humidity + ' %';
-  list.appendChild(currentHumidity);
+  currentCity.appendChild(currentHumidity);
 
   let displayCurrentWindSpeed = data.wind;
-  let currentWindSpeed = document.createElement('li');
+  let currentWindSpeed = document.createElement('h5');
   currentWindSpeed.textContent = 'Wind Speed: ' + displayCurrentWindSpeed.speed + ' MPH';
-  list.appendChild(currentWindSpeed);
-
-  // let displayCurrentUVIndex = data.main;
-  // let currentUVIndex = document.createElement('li');
-  // currentUVIndex.textContent = 'UV Index: ' + displayCurrentUVIndex;
-  // list.appendChild(currentUVIndex);
+  currentCity.appendChild(currentWindSpeed);
 
 };
+
+const displayForecast = (castData) => {
+
+  let forecast = document.querySelector('#forecast-card');
+
+
+
+
+}
 
 // fetch current weather
 const getCurrentWeather = (cityName) => {
 
-  let api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=e3b8bd27aaa26d31be8e66dd93093f79`;
+  let api = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`;
+  let castApi = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`;
 
   fetch(api)
     .then((res) => res.json())
     .then((data) => {
       displayCurrentWeather(data, cityName);
-      console.log(data, cityName);
-    });
+      console.log(data);
+    })
+  fetch(castApi)
+    .then((res) => res.json())
+    .then((castData) => {
+      console.log(castData);
+    })
 };
 
 citySubmit.addEventListener('click', citySubmitHanlder);
